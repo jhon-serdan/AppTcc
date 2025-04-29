80464a648bc20a56700a85359c1ddffef34076c5
Jonthan <jonatanm@live.com>
2025-04-29 20:39:04 -0300

    limpa tela quando volta para a tela de transações, incluído formatação por valor

--- AppTcc/Views/PaginaTransacoes.xaml.cs
+++ AppTcc/Views/PaginaTransacoes.xaml.cs
@@ -1,15 +1,25 @@
 using AppTcc.Helper;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
 
 namespace AppTcc.Views;
 
 public partial class Transacoes : ContentPage
 {
+    ObservableCollection<Transacao> lista = new ObservableCollection<Transacao>();
+
+
 	public Transacoes()
 	{
 		InitializeComponent();
-	}
 
+        lst_Transacoes.ItemsSource = lista;
+
+    }
+
+
+    #region Limpar toda tabela de Transações
     private async Task LimparTodasTransacoes()
     {
 		bool answer = await DisplayAlert("Atenção", "Deseja zerar todas as transações. Essa ação não pode ser desfeita", "Sim", "Não");
@@ -35,4 +45,24 @@ public partial class Transacoes : ContentPage
     {
 		await LimparTodasTransacoes();
     }
+    #endregion
+
+    protected async override void OnAppearing()
+    {
+        try
+        {
+            lista.Clear();
+
+            List<Transacao> tmp = await App.DB.ListarTransacaoAsync();
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "oK");
+        }
+
+        
+    }
+
 }
