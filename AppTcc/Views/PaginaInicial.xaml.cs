cb91f8877c03edfcf55160c12be15f516f233837
Jonthan <jonatanm@live.com>
2025-05-07 20:41:30 -0300

    inserção função para aparecer o total de despesa no mês

--- AppTcc/Views/PaginaInicial.xaml.cs
+++ AppTcc/Views/PaginaInicial.xaml.cs
@@ -1,14 +1,69 @@
 using AppTcc.Popups;
 using CommunityToolkit.Maui.Views;
 using Microsoft.Maui.Controls.PlatformConfiguration;
+using AppTcc.Helper;
+using System.Globalization;
 
 namespace AppTcc.Views;
 
 public partial class PaginaInicial : ContentPage
 {
+
+    private decimal _somarReceitas = 0;
+    private decimal _somarDespesas = 0;
+    private decimal _saldoTotal = 0;
+
     public PaginaInicial()
     {
         InitializeComponent();
+
+        DatePickerPagInicial.Date = DateTime.Now;
+
+        DatePickerPagInicial.DateSelected += OnDatePickerDateSelected;
+
+        CarregarDadosMes(DateTime.Now.Month, DateTime.Now.Year);
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+    }
+
+    private async void OnDatePickerDateSelected (object sender, DateChangedEventArgs e)
+    {
+        await CarregarDadosMes(e.NewDate.Month, e.NewDate.Year);
+    }
+
+    private async Task CarregarDadosMes (int mes, int ano)
+    {
+        try
+        {
+            var transacoes = await App.DB.ListarTransacaoMes(mes, ano);
+
+            _somarReceitas = transacoes
+                .Where(t => t.Tipo == TipoTransacao.Receita)
+                .Sum(t => (decimal)t.Valor);
+
+            _somarDespesas = transacoes
+                .Where(t => t.Tipo == TipoTransacao.Despesa)
+                .Sum(t => (decimal)t.Valor);
+
+            _saldoTotal = _somarReceitas - _somarDespesas;
+
+            AtualizarInterface();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", $"Erro ao carregar os dados {ex.Message}", "OK");
+        }
+    }
+
+    private void AtualizarInterface ()
+    {
+        LblSaldoTotal.Text = _saldoTotal.ToString("C", CultureInfo.GetCultureInfo("pt-BR"));
+        LblDespesaTotal.Text = _somarDespesas.ToString("C", CultureInfo.GetCultureInfo("pt-BR"));
+        LblReceitaTotal.Text = _somarReceitas.ToString("C", CultureInfo.GetCultureInfo("pt-BR"));
     }
 
     private void FloatingActionButton_Clicked(object sender, EventArgs e)
